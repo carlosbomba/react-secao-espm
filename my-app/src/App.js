645c60d38e201cb7e173733d0e5e8b14708a3c54
carlosbomba <carlostorricomendoza@gmail.com>
2022-05-22 17:54:29 -0300

    adding footer configs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Secao from './components/Secao.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <>
     <Secao />
-    {/* <Abas /> */}
+    <Footer />
     </>
   );
 }
