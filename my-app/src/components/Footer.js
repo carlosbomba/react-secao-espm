c2700d927466fc7cbfedd40ffb0459478a04053f
carlosbomba <carlostorricomendoza@gmail.com>
2022-05-22 19:26:16 -0300

    final touches for prettyness

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import '../App.css';
-import './Footer.css';
 
 function Footer() {
 
--- my-app/src/components/Secao.js
+++ my-app/src/components/Secao.js
@@ -1,6 +1,5 @@
 import React from "react";
 import '../App.css';
-import './Secao.css';
 
 function Secao() {
     return (
