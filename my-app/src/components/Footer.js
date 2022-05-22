c37a1d0c3b4a464575661129abea74683f725fc4
Ana Clara Luz <anaclarabbcluz@gmail.com>
2022-05-22 18:59:58 -0300

    alterando algumas coisinhas

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -13,42 +13,51 @@ function Footer() {
 
     return (
         <div className='footer-container'>
-            <div className="bloc-tabs">
-                <button className={toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>Tab 1
+            <div className="arba-bloqueada">
+                <button className={toggleState === 1 ? "arba arba-ativa" : "arba"} onClick={() => toggleTab(1)}>Arba 1
                 </button>
-                <button className={toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Tab 2
+                <button className={toggleState === 2 ? "arba arba-ativa" : "arba"} onClick={() => toggleTab(2)}>Arba 2
                 </button>
-                <button className={toggleState === 3 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(3)}>Tab 3
+                <button className={toggleState === 3 ? "arba arba-ativa" : "arba"} onClick={() => toggleTab(3)}>Arba 3
                 </button>
             </div>
-            <div className="content-tabs">
-            <div className={toggleState === 1 ? "content  active-content" : "content"}>
-                <h2>Content 1</h2>
-                    <hr />
+            <div className="arba-surto">
+            <div className={toggleState === 1 ? "surto surto-ativo" : "surto"}>
+                <h2>Surto 1 - Questionando a existência</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-                            praesentium incidunt quia aspernatur quasi quidem facilis quo nihil
-                            vel voluptatum?
+                        Banana is life. I want french fries. <br />
+                        My life is a mess. I want a vacation. <br />
+                        What I'm doing here? Life is meaningless. <br />
+                        I just want a break.
                         </p>
             </div>
-            <div className={toggleState === 2 ? "content  active-content" : "content"}>
-                <h2>Content 2</h2>
-                    <hr />
+            <div className={toggleState === 2 ? "surto surto-ativo" : "surto"}>
+                <h2>Surto 2 - Um gato morreu</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-                            voluptatum qui adipisci.
+                        I swallowed shampoo. Probably gonna die. <br />
+                        It smelt like fruit. That was a lie. <br />
+                        Called the number on the bottle. Spoke to a guy. <br />
+                        He said "vomit". I said "why?". <br />
+                        He said "poison". I said "goodbye". <br />
+                        I looked at my finger. And looked at my life. <br />
+                        It wasn't that much. I'll probably fine.
                         </p>
             </div>
-            <div className={toggleState === 3 ? "content  active-content" : "content"}>
-                <h2>Content 3</h2>
-                    <hr />
+            <div className={toggleState === 3 ? "surto surto-ativo" : "surto"}>
+                <h2>Surto 3 - Música para não desistir</h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
-                            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
-                            Accusamus in quia odit aspernatur provident et ad vel distinctio
-                            recusandae totam quidem repudiandae omnis veritatis nostrum
-                            laboriosam architecto optio rem, dignissimos voluptatum beatae
-                            aperiam voluptatem atque. Beatae rerum dolores sunt.
+                        We're no strangers to love <br />
+                        You know the rules and so do I (do I) <br />
+                        A full commitment's what I'm thinking of <br />
+                        You wouldn't get this from any other guy <br />
+                        I just wanna tell you how I'm feeling <br />
+                        Gotta make you understand <br />
+                        Never gonna give you up <br />
+                        Never gonna let you down <br />
+                        Never gonna run around and desert you <br />
+                        Never gonna make you cry <br />
+                        Never gonna say goodbye <br />
+                        Never gonna tell a lie and hurt you. 
                         </p>
             </div>
         </div>
